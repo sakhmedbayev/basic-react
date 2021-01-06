b779b8448f26ba8053ba28b2cd3b9289f3f22ead
Said Akhmedbayev <said.akh@gmail.com>
2021-01-06 16:05:15 +0600

    composing-components

--- src/index.js
+++ src/index.js
@@ -1,18 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+// import App from "./App";
+// import reportWebVitals from "./reportWebVitals";
 
-function tick() {
-  const element = (
-    <div>
-      <h1>Hello, world!</h1>
-      <h2>It is {new Date().toLocaleTimeString()}.</h2>
-    </div>
-  );
-  // highlight-next-line
-  ReactDOM.render(element, document.getElementById("root"));
+function Welcome(props) {
+  return <h1>Hello, {props.name}</h1>;
 }
 
-setInterval(tick, 1000);
+function App() {
+  return (
+    <div>
+      <Welcome name="Sara" />
+      <Welcome name="Cahal" />
+      <Welcome name="Edite" />
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
