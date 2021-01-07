b957f44e726e501c07ce528b18ad08a2fe0d2bf9
Said Akhmedbayev <said.akh@gmail.com>
2021-01-07 20:45:25 +0600

    useState Hook demo 2

--- src/components/UseStateDemo.js
+++ src/components/UseStateDemo.js
@@ -24,3 +24,33 @@ export default function UseStateDemo() {
     </div>
   );
 }
+
+// Play with an InitialState
+
+// Functional updates
+// If the new state is computed using the previous state, you can pass a function to setState. The function will receive the previous value, and return an updated value. Here’s an example of a counter component that uses both forms of setState:
+
+function Counter({ initialCount }) {
+  const [count, setCount] = useState(initialCount);
+  return (
+    <>
+      Count: {count}
+      <button onClick={() => setCount(initialCount)}>Reset</button>
+      <button onClick={() => setCount((prevCount) => prevCount - 1)}>-</button>
+      <button onClick={() => setCount((prevCount) => prevCount + 1)}>+</button>
+    </>
+  );
+}
+
+// setState(prevState => {
+//   // Object.assign would also work
+//   return {...prevState, ...updatedValues};
+// });
+
+// Lazy initial state
+// The initialState argument is the state used during the initial render. In subsequent renders, it is disregarded. If the initial state is the result of an expensive computation, you may provide a function instead, which will be executed only on the initial render:
+
+// const [state, setState] = useState(() => {
+//   const initialState = someExpensiveComputation(props);
+//   return initialState;
+// });
